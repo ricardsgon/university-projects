e926caf8a76c57db3afef905aac9e99e1525c181
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-02 15:44:52 -0400

    Update Bot-Test.py

---
+++ Python - ALG/Projects/Bot-Test.py
@@ -0,0 +1,10 @@
+nome = input("Olá! Qual é o seu nome?")
+print(f"Prazer em te conhecer, {nome}!")
+idade_input = input("Quantos anos você tem?")
+idade = int(idade_input)
+idade_bot = 3
+diferenca_idade = idade - idade_bot
+print(f"Você é {diferenca_idade} anos mais velho(a) do que eu. Eu tenho apenas {idade_bot} anos!")
+cor = input("Qual é a sua cor favorita?")
+cor = "azul"
+print(f"Oh, {cor} é uma cor linda!")
