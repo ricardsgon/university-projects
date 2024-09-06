67111fd12729f858567f2c3d57a0e6dbb15fdb33
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-05 22:05:54 -0400

    Update Teste1.c

--- 2° Semestre - ALG/Teste1.c
+++ 2° Semestre - ALG/Teste1.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <locale.h>
 
+#define MAX_VG 100
+
+void vogals(char vogal[]);
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int age;
-    printf("Digite sua idade: ");
-    scanf("%d", &age);
-    if (age < 16)
-    {
-        printf("Voto não permitido.");
-    }
-    else if (age > 17 && age < 71)
-    {
-        printf("Voto obrigatório");
-    }
-    else
-    {
-        printf("Voto Facultativo");
-    }
+    char string[MAX_VG];
+    printf("Insira uma frase: ");
+    fgets(string, MAX_VG, stdin);
+    vogals(string);
     return 0;
+}
+
+void vogals(char vogal[]){
+    int contadorMaior = 0;
+    int contadorMenor = 0;
+    for(int i = 0; i<MAX_VG; i++){
+        if(vogal[i] == 'a'||vogal[i] == 'e'||vogal[i] == 'i'||vogal[i] == 'o'||vogal[i] == 'u'){
+            contadorMenor++;
+        }else if(vogal[i] == 'A'||vogal[i] == 'E'||vogal[i] == 'I'||vogal[i] == 'O'||vogal[i] == 'U'){
+            contadorMaior++;
+        }
+    }
+   printf("Quantidade de Vogais Maiúsculas: %d\n", contadorMaior);
+   printf("Quantidade de Vogais Minúsculas: %d\n", contadorMenor); 
+
 }
