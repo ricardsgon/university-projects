3f21ee05e71417c3c136c813a7502e29863a14c5
Ricardo Gonçalves <162488386+ricardsgon@users.noreply.github.com>
2024-09-08 22:37:43 -0400

    Update Ficha.c

--- 2° Semestre - ALG/Contéudo/Ficha.c
+++ 2° Semestre - ALG/Contéudo/Ficha.c
@@ -3,106 +3,143 @@
 #include <locale.h>
 #include <string.h>
 
+#define MAX_PERFIL 100
+
 typedef struct dadops
 {
     char nome[50];
-    char cpf[50];
-    char estcivil[20];
-} DadoPS;
-typedef struct dadopf
-{
-    char emprego[50];
-    float salario;
+    char cpf[12];
+    int estadocivil;
     char formacao[50];
-} DadoPF;
-typedef struct dadoPP
-{
-    char esporte[40];
-    char animal[40];
-    char jogo[40];
-} DadoPP;
-typedef struct junto
-{
-    DadoPS Pessoal;
-    DadoPF Profissional;
-    DadoPP Preferencia;
-} Jun;
-Jun ler()
-{
-    Jun Registro;
-    printf("--Informações pessoais--\n");
-    printf("Nome: ");
-    scanf(" %[^\n]", Registro.Pessoal.nome);
-    printf("CPF(num): ");
-    scanf(" %[^\n]", Registro.Pessoal.cpf);
-    printf("Estado Civil: ");
-    scanf(" %[^\n]", Registro.Pessoal.estcivil);
-    printf("\n");
+} DadoPS;
 
-    printf("--Informações profissionais--\n");
-    printf("Emprego: ");
-    scanf(" %[^\n]", Registro.Profissional.emprego);
-    printf("Formação: ");
-    scanf(" %[^\n]", Registro.Profissional.formacao);
-    printf("Salário: ");
-    scanf("%f", &Registro.Profissional.salario);
-    printf("\n");
+void AddPerfil(DadoPS Perfil[], int *contador);
+void MostrarPerfils(DadoPS Perfil[], int contador);
+void MostrarPerfil(DadoPS Perfil[]);
 
-    printf("--Preferências--\n");
-    printf("Esporte favorito: ");
-    scanf(" %[^\n]", Registro.Preferencia.esporte);
-    printf("Animal favorito: ");
-    scanf(" %[^\n]", Registro.Preferencia.animal);
-    printf("Jogo favorito: ");
-    scanf(" %[^\n]", Registro.Preferencia.jogo);
-    printf("\n");
-    
-    return Registro;
-}
-void printar(Jun Registro)
-{
-    printf("\n -- FICHA PESSOAL --  \n");
-    printf("Nome: %s\n", Registro.Pessoal.nome);
-    printf("CPF: %s\n", Registro.Pessoal.cpf);
-    printf("Estado Civil: %s\n", Registro.Pessoal.estcivil);
-
-    printf("\n -- DADOS PROFISSIONAIS -- \n");
-    printf("Emprego: %s\n", Registro.Profissional.emprego);
-    printf("Formação: %s\n", Registro.Profissional.formacao);
-    printf("Salario: %.1f\n", Registro.Profissional.salario);
-
-    printf("\n -- PREFERÊNCIAS -- \n");
-    printf("Esporte favorito: %s\n", Registro.Preferencia.esporte);
-    printf("Animal favorito: %s\n", Registro.Preferencia.animal);
-    printf("Jogo favorito: %s\n", Registro.Preferencia.jogo);
-    printf("\n");
-
-}
 int main()
 {
     setlocale(LC_ALL, "Portuguese_Brazil");
-    Jun Registro = {0};
-    Jun z;
     int menu;
+    int contador = 0;
+    DadoPS Perfil[MAX_PERFIL];
     do
     {
-
-        printf("Avaliador de Perfil\n\n 1 - Criar Perfil\n 2 - Meu Perfil\n 3 - Listar todos perfils\n 4 - Encerrar\n");
+        printf("\n\n-- SISTEMA DE FICHAS --\n\n1 - Criar Perfil\n2 - Ver Perfil\n3 - Listar todos perfils\n4 - Remover Lista\n5 - Sair\n");
         printf("--> ");
         scanf("%d", &menu);
         printf("\n");
+        getchar();
         switch (menu)
         {
         case 1:
-            z = ler();
+            AddPerfil(Perfil, &contador);
             break;
-            
         case 2:
-            printar(z);
+            MostrarPerfil(Perfil);
             break;
         case 3:
-            
+            MostrarPerfils(Perfil, contador);
+            break;
+        case 4:
+            RemoverLista(Perfil, &contador);
+            break;
         }
+    } while (menu != 5);
+}
 
-    } while (menu != 4);
+void AddPerfil(DadoPS Perfil[], int *contador)
+{
+    if (*contador < MAX_PERFIL)
+    {
+        printf("Insira seu nome: ");
+        fgets(Perfil[*contador].nome, sizeof(Perfil[*contador].nome), stdin);
+        printf("Insira seu CPF: ");
+        scanf("%s", &Perfil[*contador].cpf);
+        getchar();
+        printf("Qual é seu estado civil(1 - Solteiro(a), 2 - Casado(a), 3 - Divorciado(a), 4 - Viúvo(a)): ");
+        scanf("%d", &Perfil[*contador].estadocivil);
+        getchar();
+        printf("Insira sua formação: ");
+        fgets(Perfil[*contador].formacao, sizeof(Perfil[*contador].nome), stdin);
+    }
+    else
+    {
+        printf("Limite de fichas atingido.");
+    }
+    (*contador)++;
+}
+void MostrarPerfil(DadoPS Perfil[])
+{
+    int ID;
+    printf("Digite o ID do perfil que quer exibir: ");
+    scanf("%d", &ID);
+    printf("-- FICHA (ID: %d) -- \n\n", ID);
+    printf("Nome: %s", Perfil[ID].nome);
+    printf("CPF: %s\n", Perfil[ID].cpf);
+    printf("Formação: %s", Perfil[ID].formacao);
+    if (Perfil[ID].estadocivil == 1)
+    {
+        printf("Estado Civil: Solteiro(a)\n");
+    }
+    else if (Perfil[ID].estadocivil == 2)
+    {
+        printf("Estado Civil: Casado(a)\n");
+    }
+    else if (Perfil[ID].estadocivil == 3)
+    {
+        printf("Estado Civil: Divorciado(a)\n");
+    }
+    else if (Perfil[ID].estadocivil == 4)
+    {
+        printf("Estado Civil: Viúvo(a)\n");
+    }
+    printf("\n");
+}
+void MostrarPerfils(DadoPS Perfil[], int contador)
+{
+    if (contador == 0)
+    {
+        printf("Nenhuma ficha no sistema.");
+    }
+    for (int i = 0; i < contador; i++)
+    {
+        printf("-- FICHA (ID: %d) -- \n\n", i);
+        printf("Nome: %s", Perfil[i].nome);
+        printf("CPF: %s\n", Perfil[i].cpf);
+        printf("Formação: %s", Perfil[i].formacao);
+        if (Perfil[i].estadocivil == 1)
+        {
+            printf("Estado Civil: Solteiro(a)\n");
+        }
+        else if (Perfil[i].estadocivil == 2)
+        {
+            printf("Estado Civil: Casado(a)\n");
+        }
+        else if (Perfil[i].estadocivil == 3)
+        {
+            printf("Estado Civil: Divorciado(a)\n");
+        }
+        else if (Perfil[i].estadocivil == 4)
+        {
+            printf("Estado Civil: Viúvo(a)\n");
+        }
+        printf("\n");
+    }
+}
+void RemoverLista(DadoPS Perfil[], int *contador)
+{
+    int ID;
+    printf("Insira o ID da ficha que vai ser removida: ");
+    scanf("%d", &ID);
+    for (int i = 0; i < *contador; i++)
+    {
+        if (ID <= *contador)
+        {
+            for (int j = i; j < *contador - 1; j++)
+            {
+                Perfil[j] = Perfil[j + 1];
+            }
+        }
+    }
 }
